132d21d202deb3cc7315327b3e48b3de0256228c
Julia Buzina <buzinay@mail.ru>
2018-02-13 11:40:17 +0300

    Добавила автоматизацию

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,18 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var del = require("del");
+var run = require("run-sequence");
+
+//--Собираем и минифицируем стили--
 
 gulp.task("style", function() {
   gulp.src("source/less/style.less")
@@ -14,13 +26,103 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
+
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+//--Минифицируем изображения--
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+ ]))
+  .pipe(gulp.dest("source/img"));
+});
+
+//--Webp--
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("source/img"));
+});
+
+//--собираем спрайт--
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/img"));
+});
+
+//--Минимизируем js--
+
+gulp.task("js", function() {
+  return gulp.src(["source/js/*.js", "!source/js/*.min.js"])
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: ".min"
+    }))
+    .pipe(gulp.dest("build/js"))
+});
+
+//--Запускаем Posthtml
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+  .pipe(posthtml([
+    include()
+  ]))
+  .pipe(server.stream())
+  .pipe(gulp.dest("build"));
+});
+
+//--Очищаем build--
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+//--Копируем в build--
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**"
+    ], {
+      base: "source"
+    })
+  .pipe(gulp.dest("build"))
+});
+
+//--Запуск последовательности--
+
+gulp.task("build", function(done) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "js",
+    "sprite",
+    "html",
+    done
+    );
+});
+
+gulp.task("serve", function() {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -28,5 +130,5 @@ gulp.task("serve", ["style"], function() {
   });
 
   gulp.watch("source/less/**/*.less", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html", ["html"]).on("change", server.reload);
 });
